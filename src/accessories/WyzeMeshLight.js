a19bb8ba3dabd3f313d8e8f67b70b0d5d83aa6f2
Jake Lehner <jakelehner@gmail.com>
2021-08-17 19:09:47 -0600

    .

--- src/accessories/WyzeMeshLight.js
+++ src/accessories/WyzeMeshLight.js
@@ -57,13 +57,15 @@ module.exports = class WyzeMeshLight extends WyzeAccessory {
 
   updateColorTemp(value) {
     if (value == '') {
-      value = 0;
+      return false;
     }
     
     let floatValue = this._rangeToFloat(value, WYZE_COLOR_TEMP_MIN, WYZE_COLOR_TEMP_MAX);
     let homeKitValue = this._floatToRange(floatValue, HOMEKIT_COLOR_TEMP_MIN, HOMEKIT_COLOR_TEMP_MAX);
-    this.plugin.log.info(`updateColorTemp for ${this.homeKitAccessory.context.mac} (${this.homeKitAccessory.context.nickname}) to ${value}`);
-    this.plugin.log.info(`${value} -> ${floatValue} -> ${homeKitValue}`);
+
+    this.plugin.log.debug(`updateColorTemp for ${this.homeKitAccessory.context.mac} (${this.homeKitAccessory.context.nickname}) to ${value}`);
+    this.plugin.log.debug(`${value} -> ${floatValue} -> ${homeKitValue}`);
+    
     this.getCharacteristic(Characteristic.ColorTemperature).updateValue(homeKitValue);  
   }
 
